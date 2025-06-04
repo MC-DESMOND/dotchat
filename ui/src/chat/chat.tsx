e26f8db1d824458a0eb1d8247f0a88d4740d4694
MC_DESMOND <macdesmonddikeibeh@gmail.com>
2025-06-04 13:30:32 +0100

    gffgfgf

---
+++ ui/src/chat/chat.tsx
@@ -0,0 +1,12 @@
+import BaseHOC from "../addons/HOC";
+
+
+
+export function Chat(){
+    const base = new BaseHOC()
+
+    return <base._>
+    
+    </base._>
+
+}
---
+++ ui/src/home/home.tsx
@@ -0,0 +1,10 @@
+import BaseHOC from "../addons/HOC"
+
+
+
+export default function Home(){
+    const base = new BaseHOC()
+    return <base._>
+        
+    </base._>
+}
